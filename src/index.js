7df5323704db3d7599e3859678fcecda0d9c9297
Gyana Ranjan panda <83230804+lonewolf2002@users.noreply.github.com>
2022-05-09 17:19:57 +0530

    intial commit

--- src/index.js
+++ src/index.js
@@ -1,18 +1,35 @@
-const getHumanReadableSizeObject = (bytes) => {
-    if (bytes === 0) {
-      return {
-        size: 0,
-        measure: 'Bytes'
-      }
-    }
-  
-    const K = 1024
-    const MEASURE = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
-    const i = Math.floor(Math.log(bytes) / Math.log(K))
-  
-    return {
-      size: parseFloat((bytes / Math.pow(K, i)).toFixed(2)),
-      measure: MEASURE[i]
-    }
+const apiId = "https://api.github.com/repos";
+//Syntax GET /repos/:user/:repo
+
+let URL = location.pathname;
+let size;
+
+//KB TO MB
+function formatBytes(bytes, decimals = 2) {
+  if (bytes === 0) return "0 Bytes";
+
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = [ "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
+}
+
+//data fetching
+const FetchSize = async (url) => {
+  try {
+    const res = await fetch(`${apiId}${url}`);
+    const Data = await res.json();
+    size = Data.size;
+  } catch (error) {
+    console.error(error);
   }
-  console.log(getHumanReadableSizeObject(1064))
+};
+//injecting to DOM
+FetchSize(URL).then(() => {
+  const UL = document.querySelector("#repository-container-header > nav > ul");
+  const li = `<li class="d-inline-flex">SIZE: ${formatBytes(size)}</li>`;
+  UL.innerHTML += li;
+});
